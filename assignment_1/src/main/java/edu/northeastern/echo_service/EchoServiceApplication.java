82742983052544a338d4faa284d29a4283ed602b
1v17 <166570730+1v17@users.noreply.github.com>
2025-07-30 15:41:32 -0700

    add springboot file

---
+++ assignment_1/src/main/java/edu/northeastern/echo_service/EchoServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.northeastern.echo_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoServiceApplication.class, args);
+	}
+
+}
---
+++ assignment_1/src/main/java/edu/northeastern/echo_service/EchoServiceController.java
@@ -0,0 +1,18 @@
+package edu.northeastern.echo_service;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/echo")
+public class EchoController {
+    
+    @PostMapping
+    public String echo(@RequestBody String message) {
+        return message;
+    }
+    
+    @GetMapping
+    public String echoGet(@RequestParam String message) {
+        return message;
+    }
+}
